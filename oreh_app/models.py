f5a35634c0683693ec3623c73cf4bcb7ea6db638
Tikhon Klyshko <tihon.klyshko@gmail.com>
2020-11-24 19:13:02 +0300

    New model Activity

--- oreh_app/models.py
+++ oreh_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Activity(models.Model):
+    name = models.CharField("Имя", max_length=100)
+    description = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="Activity/")
+    location = models.TextField("место")
+    organizer = models.ManyToManyField(Organizer, verbose_name="организатор")
+    places = models.CharField("кол-во мест")
+    date = models.TextField("дата")
+
