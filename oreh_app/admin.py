e6874f537b5704a7c63049dd2b18aea641e2da3f
Tikhon Klyshko <tihon.klyshko@gmail.com>
2020-11-26 22:04:25 +0300

    Register models in admin

--- oreh_app/admin.py
+++ oreh_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from.models import BusinessModel, FinancialPlan, Team, Activity, Person, Courses, Participant, Project,  Services, FieldOfActivity, Achievements, Competitors
 
-# Register your models here.
+
+admin.site.register(BusinessModel)
+admin.site.register(FinancialPlan)
+admin.site.register(Team)
+admin.site.register(Activity)
+admin.site.register(Person)
+admin.site.register(Courses)
+admin.site.register(Participant)
+admin.site.register(Project)
+admin.site.register(Services)
+admin.site.register(FieldOfActivity)
+admin.site.register(Achievements)
+admin.site.register(Competitors)
+
