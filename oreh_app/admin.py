f191d52e6f394cb46ad0c4cea971830788a8e883
MR Nrash <romaschezhin@gmail.com>
2020-12-01 20:06:26 +0400

    +models

--- oreh_app/admin.py
+++ oreh_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from oreh_app.models import FieldOfActivity, Person, Participant, Courses, Team, Graduate, Resident, NatPer, Workers, \
+    LegalEntity
+
+admin.site.register(FieldOfActivity)
+admin.site.register(Person)
+admin.site.register(Participant)
+admin.site.register(Courses)
+admin.site.register(Team)
+admin.site.register(Graduate)
+admin.site.register(Resident)
+admin.site.register(NatPer)
+admin.site.register(Workers)
+admin.site.register(LegalEntity)
