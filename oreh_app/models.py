223a7bf739d23d07b06c8ffbcac0007c87c18e9c
MR Nrash <romaschezhin@gmail.com>
2020-12-05 16:44:30 +0400

    +str

--- oreh_app/models.py
+++ oreh_app/models.py
@@ -5,16 +5,13 @@ from django.db import models
 class FieldOfActivity(models.Model):
     name = models.CharField(max_length=128)
 
+    def __str__(self):
+        return self.name
+
 
 # Лицо
 class Person(models.Model):
     field_of_activity = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
-
-
-# Участник
-class Participant(models.Model):
-    photo = models.ImageField()
-    description = models.TextField()
 
 
 # Курсы
@@ -29,6 +26,21 @@ class Courses(models.Model):
     requirement = models.TextField()
     field_of_activity = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.name
+
+
+# Участник
+class Participant(models.Model):
+    photo = models.ImageField()
+    description = models.TextField()
+    per = models.OneToOneField(Person, on_delete=models.CASCADE)
+    team = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
+    courses = models.ManyToManyField(Courses, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.description
+
 
 # Команда
 class Team(models.Model):
@@ -36,12 +48,19 @@ class Team(models.Model):
     title = models.CharField(max_length=128)
     field_of_activity = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.title
+
 
 # Выпускник
 class Graduate(models.Model):
     photo = models.ImageField()
     description = models.TextField()
     team = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
+    per = models.OneToOneField(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.per
 
 
 # Резидент
@@ -50,6 +69,9 @@ class Resident(models.Model):
     second_name = models.CharField(max_length=128)
     team = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
     person = models.OneToOneField(Person, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} {self.second_name}"
 
 
 # Физическое лицо
@@ -61,10 +83,16 @@ class NatPer(models.Model):
     team = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
     per = models.OneToOneField(Person, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.name} {self.second_name}"
+
 
 # Работники
 class Workers(models.Model):
     nat_per = models.OneToOneField(NatPer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nat_per.name
 
 
 # Юр. Лицо
@@ -72,3 +100,6 @@ class LegalEntity(models.Model):
     per = models.OneToOneField(Person, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     team = models.ForeignKey(FieldOfActivity, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
