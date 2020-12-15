523524289731d428ccc577a1e2f58eb2138657e8
MR Nrash <romaschezhin@gmail.com>
2020-12-15 20:41:42 +0400

    +something

--- oreh_app/views.py
+++ oreh_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from oreh_app.models import Achievement
+
 
 class IndexView(View):
-    def get(self,request):
-        return render(request, 'index.html')
+    def get(self, request):
+        achievements = Achievement.objects.all()
+        return render(request, 'index.html', {'achievements', achievements})
