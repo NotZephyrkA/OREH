5a39e7876e96a3e99224f535495b0602b680707d
Jovenavr0 <vvkarpenko2005@mail.ru>
2020-12-15 20:23:45 +0400

    добавил страницу текущие проекты

--- oreh_app/urls.py
+++ oreh_app/urls.py
@@ -5,4 +5,5 @@ from oreh_app import views
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path('residents/', views.ResidentsView.as_view()),
+    path('current_project/', views.CurrentProjectsView.as_view()),
 ]
--- oreh_app/views.py
+++ oreh_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
-from oreh_app.models import Resident
+from oreh_app.models import Resident, Project
 
 
 class IndexView(View):
@@ -13,3 +13,9 @@ class ResidentsView(View):
     def get(self, request):
         residents = Resident.objects.all()
         return render(request, 'residents.html', {'residents': residents})
+
+
+class CurrentProjectsView(View):
+    def get(self, request):
+        current_projects = Project.objects.all()
+        return render(request, 'current projects.html', {'current projects': current_projects})
