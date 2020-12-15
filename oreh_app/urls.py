6edba52be5600dd3ec9b4c7bdc39681c2bcce282
Tikhon Klyshko <tihon.klyshko@gmail.com>
2020-12-15 19:27:21 +0300

    Добавил страницу выпускников

--- oreh_app/urls.py
+++ oreh_app/urls.py
@@ -4,4 +4,5 @@ from oreh_app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view()),
+    path('graduates/', views.GraduatesView.as_view()),
 ]
--- oreh_app/views.py
+++ oreh_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from oreh_app.models import Graduate
+
 
 class IndexView(View):
     def get(self,request):
         return render(request, 'index.html')
+
+
+class GraduatesView(View):
+    def get(self, request):
+        graduates = Graduate.objects.all()
+        return render(request, 'graduates.html', {'graduates': graduates})
